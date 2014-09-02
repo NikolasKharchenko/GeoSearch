ca80b005236e298b114f0dc221bad44b04e5d4ee
Николай <nikolaj@Mac-mini-Nikolaj.local>
2014-09-02 11:54:27 +0400

    Initial Commit

---
+++ GeoSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeoSearch
+//
+//  Created by Николай on 02.09.14.
+//  Copyright (c) 2014 Николай. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
